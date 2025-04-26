b700fe2a8c0b659d615130e1277de3f9948c999e
iznobe <iznobe@gmail.com>
2025-04-26 10:02:28 +0200

    Update automount_part_data.sh

--- automount_part_data.sh
+++ automount_part_data.sh
@@ -12,14 +12,15 @@ LC_ALL=C
 
 label() {
   local rgx="[^[:alnum:]_-]"
+  local regLab="^($Label)$"
 
   while [ -z "$Label" ]; do
     read -rp "Choisissez l’étiquette (LABEL) de votre partition de données, elle doit être UNIQUE et ne pas contenir d’espace, d’accent, de caractères spéciaux et au maximum 16 caractères : " Label
     if [[ $Label =~ $rgx || ${#Label} -gt 16 ]]; then
       echo "Le nom de votre étiquette comporte une espace, un accent ou un caractère spécial ou plus de 16 caractères !"
       unset Label
-    elif lsblk -no label | grep -q "$Label"; then
-      echo "Erreur, votre étiquette « $Label » est déjà attribuée ! Choisissez en une autre."
+    elif [[ $(lsblk -no label) =~ $regLab ]]; then
+      echo "Erreur, votre étiquette « $Label » est déjà attribuée ! Choisissez-en une autre."
       unset Label
     fi
   done
@@ -28,7 +29,7 @@ label() {
 unmount() {
   local rgx="^(/mnt/|/media/).+$"
   local mp
-  mp="$(grep "$Label" /etc/fstab | cut -d ' ' -f 2)"
+  mp="$(grep -E "$regFstabLab" /etc/fstab | cut -d ' ' -f 2)"
 
   while true; do
     read -rp "Voulez-vous démonter la partition « $Part » de son emplacement actuel et procéder aux changements pour la monter avec l'étiquette « $Label » ? [O/n] "
@@ -38,27 +39,34 @@ unmount() {
         exit 0
       ;;
       Y|y|O|o|"")
+        # traitement des partitions montées :
         mapfile -t PartMountPoints < <(grep "$Part" /etc/mtab | cut -d " " -f 2)
         if [ -n "${PartMountPoints[0]}" ]; then
           for pmp in "${PartMountPoints[@]}"; do
             umount -v "$pmp"
-            if [[ -d $pmp && $pmp =~ $rgx ]]; then
-              rmdir -v "$pmp"
-            else
-              echo "$pmp n’a pas été supprimé."
+            if [ -d "$pmp" ]; then
+              if [[ $pmp =~ $rgx ]]; then
+                rmdir -v "$pmp"
+              else
+                echo "$pmp a été conservé."
+              fi
             fi
             mapfile -t numLines < <(grep -n "$pmp" /etc/fstab | cut -d ":" -f 1 | sort -rn)
             for n in "${numLines[@]}"; do
               sed -i "${n}d" /etc/fstab
             done
           done
-        elif [[ -d $mp && $mp =~ $rgx ]]; then
+        # traitement des partitions NON montées :
+        elif [ -d "$mp" ]; then
+          echo "$mp"
+          if [[ $mp =~ $rgx ]]; then
             rmdir -v "$mp"
-        else
-          echo "$mp n’a pas été supprimé."
-        fi
+          else
+            echo "$mp a été conservé."
+          fi
         sed -i "/$(lsblk -no uuid "$Part")/d" /etc/fstab
-        sed -i "/$Label/d" /etc/fstab
+        sed -i "/$regFstabLab/d" /etc/fstab 
+        fi
         sleep 1 # Prise en compte du montage par le dash, sans délai, parfois la partition ne s’affiche pas.
         break
       ;;
@@ -69,14 +77,16 @@ unmount() {
 }
 
 if ((UID)); then
-  echo "Vous devez être super utilisateur pour lancer ce script (essayez avec « sudo »)"
+  echo "Vous devez être super utilisateur pour lancer ce script (essayez avec « sudo »)."
   exit 1
 fi
 
 declare -A ListPart
 declare -A Rgx=( [fstype]="^(ext[2-4]|ntfs)" [mountP]="^(/|/boot|/home|/tmp|/usr|/var|/srv|/opt|/usr/local)$" )
+regFstabLab="^(LABEL=|/dev/disk/by-label/)$Label$"
 
 i=-1
+
 
 while read -ra lsblkDT; do #path fstype mountpoint label
   if [[ ${lsblkDT[1]} =~ ${Rgx[fstype]} ]]; then
@@ -97,7 +107,7 @@ while read -ra lsblkDT; do #path fstype mountpoint label
   fi
 done < <(lsblk -no path,fstype,mountpoint,label)
 
-if (( ${#ListPart[@]} == 0 )); then
+if ((${#ListPart[@]} == 0)); then
   echo "Il n’y a pas de partition susceptible d’être montée."
   exit 2
 fi
@@ -106,10 +116,10 @@ nbDev=$(("${#ListPart[@]}"/4))
 
 echo
 echo " n° ⇒    path    label   fstype   mountpoint"
-echo "-----------------------------------------------------------------"
+echo "--------------------------------------------"
 for (( n=0; n<nbDev; n++ )); do
   if ((n+1 < 10)); then
-    echo " $((n+1))  ⇒ ${ListPart[$n,0]}   ${ListPart[$n,3]}   ${ListPart[$n,1]}   ${ListPart[$n,2]}"  
+    echo " $((n+1))  ⇒ ${ListPart[$n,0]}   ${ListPart[$n,3]}   ${ListPart[$n,1]}   ${ListPart[$n,2]}"
   else
     echo " $((n+1)) ⇒ ${ListPart[$n,0]}   ${ListPart[$n,3]}   ${ListPart[$n,1]}   ${ListPart[$n,2]}"
   fi
@@ -118,7 +128,7 @@ echo
 
 while [ -z "$PartNum" ]; do
   read -rp "Choisissez le numéro correspondant à votre future partition de données : " PartNum
-  if [[ ! $PartNum =~ ^[1-9][0-9]*$ ]] || ! (( PartNum > 0 && PartNum <= nbDev )); then
+  if ! [[ $PartNum =~ ^[1-9][0-9]*$ ]] || ! ((PartNum > 0 && PartNum <= nbDev)); then
     echo "Votre choix doit être un nombre entier compris entre 1 et $nbDev."
     unset PartNum
   fi
@@ -127,6 +137,8 @@ done
 Part="${ListPart[$((PartNum-1)),0]}"
 PartLabel="${ListPart[$((PartNum-1)),3]}"
 PartFstype="${ListPart[$((PartNum-1)),1]}"
+
+#umount -v /media/"$PartLabel"
 
 if [ -z "$PartLabel" ]; then
   echo "La partition « $Part » n’a pas d’étiquette."
@@ -162,7 +174,7 @@ while true; do
       if grep -q "$(lsblk -no uuid "$Part")" /etc/fstab; then
         echo "L’UUID de la partition est déjà présent dans le fstab !"
         unmount        
-      elif grep -Eq "(LABEL=|by-label/)$Label" /etc/fstab; then
+      elif grep -Eq "$regFstabLab" /etc/fstab; then # elif grep -Eq "(LABEL=|by-label/)$Label" /etc/fstab; then
         echo "L’étiquette « $Label » est déjà utilisée dans le fstab !"
         unmount
       elif grep -q "^$Part" /etc/mtab; then
@@ -182,9 +194,8 @@ while true; do
         mkdir -v /media/"$Label"
       fi
       systemctl daemon-reload
-      if ! mount -a
-      then
-        exit 4
+      if ! mount -a; then
+        exit 3
       fi
 
       if ! [ -d /media/"$Label"/"$SUDO_USER"-"$Label" ]; then
@@ -204,8 +215,8 @@ while true; do
         echo
         echo "Vous pouvez maintenant accéder à votre partition en parcourant le dossier suivant : « /media/$Label/$SUDO_USER-$Label »."
       else
-        echo "erreur inconnue !"
-        exit 5
+        echo "Erreur inconnue !"
+        exit 4
       fi
       break
     ;;
