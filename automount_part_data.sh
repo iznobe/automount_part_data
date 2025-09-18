30848fcce070edbd8327fa7e0d3fdf6f291a35eb
iznobe <iznobe@gmail.com>
2025-09-18 11:14:10 +0200

    Update automount_part_data.sh

--- automount_part_data.sh
+++ automount_part_data.sh
@@ -104,9 +104,9 @@ nbDev=$(("${#ListPart[@]}"/5))
 echo "-----------------------------------------------------------------------------"
 for (( n=0; n<nbDev; n++ )); do
   if ((n+1 < 10)); then
-    echo " $((n+1))  ⇒ ${ListPart[$n,0]}   ${ListPart[$n,1]}   ${ListPart[$n,2]}   ${ListPart[$n,3]}   ${ListPart[$n,4]}"
+    echo "   $((n+1))  ⇒ ${ListPart[$n,0]}    ${ListPart[$n,1]}            ${ListPart[$n,2]}      ${ListPart[$n,3]}      ${ListPart[$n,4]}"
   else
-    echo " $((n+1)) ⇒ ${ListPart[$n,0]}   ${ListPart[$n,1]}   ${ListPart[$n,2]}   ${ListPart[$n,3]}   ${ListPart[$n,4]}"
+    echo "   $((n+1)) ⇒ ${ListPart[$n,0]}    ${ListPart[$n,1]}            ${ListPart[$n,2]}      ${ListPart[$n,3]}      ${ListPart[$n,4]}"
   fi
 done
 echo
