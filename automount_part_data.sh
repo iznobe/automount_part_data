1a5309711c520061c54cf8d20c0f8792ee24e17a
iznobe <iznobe@gmail.com>
2025-11-02 08:14:46 +0100

    Update automount_part_data.sh

--- automount_part_data.sh
+++ automount_part_data.sh
@@ -502,11 +502,11 @@ if test "$do_change" = "yes"; then
   sudo -u "$SUDO_USER" ls -l  | sudo -u "$SUDO_USER" tee -a "$log" > /dev/null
 fi
 echo
-test "$do_change" = "yes" && echo "pour voir l'état des fichiers modifiés passer la commande suivante : cat automount.log$now_time"
-echo
-echo -e "\\033[0;33m ----------------------------------------------------------------------------- \\033[0;0m"
+test "$do_change" = "yes" && info "pour voir l'état des fichiers modifiés passer la commande suivante : cat automount.log$now_time"
 echo
 blue "Script pour montage de partition de données terminé avec succès !"
+echo
+echo -e "\\033[0;33m ----------------------------------------------------------------------------- \\033[0;0m"
 echo
 info "Vous devez relancer la session graphique ou redémarrer l ' ordinateur pour que les changements soient effectifs."
 echo -e "\\033[1;31m ! IMPORTANT ! : Toutes vos données utilisateurs seront dorénavant stockées dans votre partition $newLabel : « $Part » .
